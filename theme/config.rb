7b092a146a9994a32abe3c953ad5af9db6a5f147
Maximiliano Fierro <elmasse@gmail.com>
2011-10-03 21:11:17 -0300

    first commit to gh pages for Ext.ux.Cover

---
+++ theme/config.rb
@@ -0,0 +1,8 @@
+# Get the directory that this configuration file exists in
+dir = File.dirname(__FILE__)
+
+# Compass configurations
+sass_path    = dir
+css_path     = File.join(dir, "..", "css")
+environment  = :production
+output_style = :compressed
