6f02ff50f2d91d6de502b572e3d4c85d1768a3b6
Maximiliano Fierro <elmasse@gmail.com>
2012-07-23 20:18:19 -0700

    testing some new structure

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 Ext.require('Ext.ux.Cover');
 
 Ext.application({
-	name: 'Cover',
+	name: 'CoverDemo',
 	
 	viewport: {
 		autoMaximize: true
@@ -9,65 +9,69 @@ Ext.application({
 
 	launch: function(){
 
-		var cover = Ext.create('Ext.ux.Cover', {
-			itemCls: 'my-cover-item',
-			//These are just for demo purposes.
-			height: (Ext.os.deviceType !== 'Phone')? 300: undefined,
-			width: (Ext.os.deviceType !== 'Phone')? 800: undefined,
-			//end-demo
-		    itemTpl : [
-				'<div>',
-					'<div class="dev">{firstName} {lastName}</div>',
-					'<div class="company">{company}</div>',
-					'<div class="image"><tpl if="image"><img  src="{image}"></tpl></div>',
-				'</div>'
-			],
-			store : {
-			    fields: ['firstName', 'lastName', 'company', 'image'],
-			    data: [
-			        {firstName: 'Tommy',   lastName: 'Maintz', company: 'Sencha', image: './images/sencha.png'},
-			        {firstName: 'Rob',     lastName: 'Dougan', company: 'Sencha', image: './images/sencha.png'},
-			        {firstName: 'Max',     lastName: 'Fierro', company: 'Sencha', image: './images/sencha.png'},
-			        {firstName: 'Ed',      lastName: 'Spencer', company: 'Sencha', image: './images/sencha.png'},
-			        {firstName: 'Jamie',   lastName: 'Avins', company: 'Sencha', image: './images/sencha.png'},
-			        {firstName: 'Aaron',   lastName: 'Conran', company: 'Sencha', image: './images/sencha.png'},
-			        {firstName: 'Dave',    lastName: 'Kaneda', company: 'Sencha', image: './images/sencha.png'},
-	   		        {firstName: 'Michael', lastName: 'Mullany', company: 'Sencha', image: './images/sencha.png'},
-	   		        {firstName: 'Abraham', lastName: 'Elias', company: 'Sencha', image: './images/sencha.png'},
-				    {firstName: 'Jay',     lastName: 'Robinson', company: 'Sencha', image: './images/sencha.png'}
-			    ]
-			},
-			selectedIndex: 2,
-			listeners:{
-				itemdoubletap: function(){
-					console.log('itemdbltap', arguments);
-				},
-				itemtap: function(cover, idx){
-					console.log('itemtap', arguments);
-				},
-				scope: this
-			}
-		});
+        Ext.Viewport.add({
+            xtype: 'tabpanel',
+            tabBarPosition: 'bottom',
+            items:[{
+                xtype: 'titlebar',
+                title: 'Cover Demo',
+                docked: 'top'
+            },{
+                title: 'Horizontal',
+                iconCls: 'favorites',
+                layout: 'fit',
+                items: [{
+                    xtype: 'cover',
+                    itemTpl : [
+                        '<div>',
+                            '<div class="dev">{firstName} {lastName}</div>',
+                            '<div class="company">{company}</div>',
+                            '<div class="image"><tpl if="image"><img  src="{image}"></tpl></div>',
+                        '</div>'
+                    ],
+                    store : {
+                        storeId: 'store1',
+                        fields: ['firstName', 'lastName', 'company', 'image'],
+                        data: [
+                            {firstName: 'Tommy',   lastName: 'Maintz', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Rob',     lastName: 'Dougan', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Max',     lastName: 'Fierro', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Ed',      lastName: 'Spencer', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Jamie',   lastName: 'Avins', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Aaron',   lastName: 'Conran', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Dave',    lastName: 'Kaneda', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Michael', lastName: 'Mullany', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Abraham', lastName: 'Elias', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Jay',     lastName: 'Robinson', company: 'Sencha', image: './images/sencha.png'}
+                        ]
+                    }
+                }]
+            },{
+                title: 'Vertical',
+                iconCls: 'favorites',
+                layout: 'fit',
+                items: [{
+                    xtype: 'cover',
+                    itemTpl : [
+                        '<div>',
+                            '<div class="dev">{firstName} {lastName}</div>',
+                            '<div class="company">{company}</div>',
+                            '<div class="image"><tpl if="image"><img  src="{image}"></tpl></div>',
+                        '</div>'
+                    ],
+                    store : {
+                        storeId: 'store2',
+                        fields: ['firstName', 'lastName', 'company', 'image'],
+                        data: [
+                            {firstName: 'Dave',    lastName: 'Kaneda', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Michael', lastName: 'Mullany', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Abraham', lastName: 'Elias', company: 'Sencha', image: './images/sencha.png'},
+                            {firstName: 'Jay',     lastName: 'Robinson', company: 'Sencha', image: './images/sencha.png'}
+                        ]
+                    }
+                }]
+            }]
 
-				
-		var tab = Ext.create('Ext.tab.Panel',{
-			tabBarPosition: 'bottom',
-			items:[{
-				title: 'cover',
-				iconCls: 'favorites',
-				//Demo purpose
-				layout: (Ext.os.deviceType === 'Phone')? 'fit': {
-					type: 'vbox',
-					pack:'center',
-					align: 'center'
-				},
-				//end demo
-				items: [cover]
-			}]
-		});
-		
-		//weird fix to call refresh when orientation changes
-	//	Ext.Viewport.on('orientationchange', function(){cover.refresh();})
-		Ext.Viewport.add(tab);
+        });
 	}
 });
--- ux/Cover.js
+++ ux/Cover.js
@@ -1,101 +1,64 @@
 /**
  * @class Ext.ux.Cover
- * @extend Ext.DataView
  *
- * A Cover represents elements in a Store as visual elements in a Coverflow-like widget.
- * @author Maximiliano Fierro
- * @notes Inspired on zflow: http://css-vfx.googlecode.com/ By Charles Ying
-*/
-Ext.define('Ext.ux.Cover',{
-    extend: 'Ext.DataView',
-    xtype: 'cover',
+ */
+Ext.define('Ext.ux.Cover', {
+    extend : 'Ext.Container',
+    xtype  : 'cover',
 
-    config:{
-       /**
-         * @cfg {Number} selectedIndex The idx from the Store that will be active first. Only one item can be active at a
-         * time
-         * @accessor
-         * @evented
-         */
-        selectedIndex: 0,
-    
-        /**
-         * @cfg {String} itemCls
-         * A css class name to be added to each item element.
-         */
-        itemCls: '',
+    require: [
+        'Ext.data.StoreManager'
+    ],
 
-        /**
-         * @cfg {Boolean} preventSelectionOnItemTap
-         * Prevent selection when item is tapped. This is false by default.
-         */
-        preventSelectionOnItemTap: false,
+    config : {
+        //private
+        // baseCls : 'ux-cover',
 
-        /**
-         * @cfg {Number} angle for cover background items
-         * This is the angle that not selected items are moved in space.
-         */     
-        angle: 70,
-        
-        /**
-         * @cfg {Boolean} set to true if you want a flat representation. Defaults to false so the
-         * coverflow remains 3d.
-         */
-        flat: false,
+        store : undefined,
 
-        /**
-         * @cfg {Boolean} preventOrientationChange
-         * Prevent attaching refresh method to orientation change event on Ext.Viewport
-         */
-        preventOrientationChange: false,
+        itemTpl: undefined,
+        //private
+        scrollable : false,
+
+        listConfig: {
+            xtype: 'container',
+            baseCls: 'ux-cover-list'
+        }
+  
+     },
+
  
-        //private
-        baseCls: 'ux-cover',
-        //private
-        itemBaseCls: 'ux-cover-item-inner',
-        //private
-        scrollable: false,
-        //private
-        orientation: undefined
+    storeEvents: {
+        'addrecords'    : 'onStoreAdd',
+        'beforeload'    : 'onStoreBeforeLoad',
+        'load'          : 'onStoreLoad',
+        'refresh'       : 'onStoreRefresh',
+        'removerecords' : 'onStoreRemove'
     },
-    
-    offset: 0,
-    
-    //override
-    initialize: function(){
-        //we need somehow to put the itemCls to the tpl wraper element 
-        this.innerItemCls = this.getItemCls();
-        if(this.innerItemCls) {
-            this.setItemCls(this.innerItemCls+'-wrap');
-        }
 
-        this.callParent();
-        
-        this.element.on({
-            drag: 'onDrag',
-            dragstart: 'onDragStart',
-            dragend: 'onDragEnd',
-            scope: this
-        });
-        
-        this.on({
-            painted: 'onPainted',
-            itemtap: 'doItemTap',
-            scope: this
-        }); 
-
-        if(!this.getPreventOrientationChange()){
-            //subscribe to orientation change on viewport
-            Ext.Viewport.on('orientationchange', this.refresh, this);
-        }
-
-        this.setItemTransformation = (this.getFlat())?this.setItemTransformFlat:this.setItemTransform3d;
+    constructor : function() {
+        var me = this;
+        me.callParent(arguments);
     },
-    
+
+    initialize : function() {
+        var me = this,
+            list;
+
+        list = me.list = me.add(me.getListConfig());
+
+        me.callParent(arguments);
+
+        //if store is already loaded no event is fired so we need to force it.
+        if (me.getStore()) {
+            me.onStoreRefresh();
+        }
+    },
 
     getElementConfig: function(){
         return {
             reference: 'element',
+            className: 'ux-cover',
             children:[{
                 reference: 'innerElement',
                 className: 'ux-cover-scroller'
@@ -103,229 +66,89 @@ Ext.define('Ext.ux.Cover',{
         }
     },
 
-    applyFlat: function(flat) {
-        return (Ext.os.is('Android')? true : flat); 
-    },
-
-    updateOrientation: function(newOrientation, oldOrientation) {
-        var baseCls = this.getBaseCls();
-        if(this.element && newOrientation != oldOrientation) {
-            this.element.removeCls(baseCls+'-'+oldOrientation);
-            this.element.addCls(baseCls+'-'+newOrientation);
-        }
-    },
-
-    applyItemTpl: function(config){
-        if(Ext.isArray(config)){
-            config = config.join("");
-        }
-        return new Ext.XTemplate('<div class="' + this.getItemBaseCls() + ' ' + this.getItemCls() + ' ">'+config+'</div>');
-    },
-
-    onPainted: function(){
-        this.refresh(); 
-    },
-
-    //private
-    getTargetEl: function(){
-        return this.innerElement;   
-    },
-
-    onDragStart: function(){
-        this.getTargetEl().dom.style.webkitTransitionDuration = "0s";
-    },
-
-    onDrag: function(e){
-        var curr = this.getOffset(),
-            offset,
-            ln = this.getViewItems().length,
-            selectedIndex,
-            delta = e.previousDeltaX;
-
-        //slow down on border conditions
-        selectedIndex = this.getSelectedIndex();
-        if((selectedIndex === 0 && e.deltaX > 0) || (selectedIndex === ln - 1 && e.deltaX < 0)){
-            delta.x *= 0.5;
-        }
-
-        offset = delta + curr;
-        
-        this.setOffset(offset, true);   
-    },
-
-    onDragEnd: function(){
-        var idx = this.getSelectedIndex(),
-            x = - (idx * this.gap);
-        this.getTargetEl().dom.style.webkitTransitionDuration = "0.4s";
-        //this.setOffset(x);
-        this.applySelectedIndex(idx);
-    },
-    
-    doItemTap: function(cover, index, item, evt){
-        if(!this.getPreventSelectionOnItemTap() && this.getSelectedIndex() !== index){
-            this.setSelectedIndex(index);
-        }
-    },
-
-    getSelectedIndex: function(){
-        var idx, ln;
-        if(this.isRendered()){
-            ln = this.getViewItems().length;
-            idx = - Math.round(this.getOffset() / this.gap);
-            this.selectedIndex = Math.min(Math.max(idx, 0),  ln - 1);
-        }
-        return this.selectedIndex;
-    },
-
-    applySelectedIndex: function(idx){
-        if(this.isRendered()){
-            this.updateOffsetToIdx(idx);
-            this.selectWithEvent(this.getStore().getAt(idx));
-        }else{
-            this.selectedIndex = idx;
-        }
-    },
-
-    updateOffsetToIdx: function(idx){
-        var ln = this.getViewItems().length,
-            offset;
-        
-        idx = Math.min(Math.max(idx, 0), ln - 1);
-        offset= -(idx * this.gap);
-        this.setOffset(offset); 
-    },
-
-    setOffset: function(offset){
-        var items = this.getViewItems(),
-            idx = 0, 
-            l = items.length,
-            item;
-        this.offset = offset;
-        this.getTargetEl().dom.style.webkitTransform = "translate3d(" + offset + "px, 0, 0)";
-        for(;idx<l;idx++){
-            item = Ext.get(items[idx]);
-            this.setItemTransformation(item, idx, offset);
-        }
-    },
-
-    getOffset: function(){
-        return this.offset;
-    },
-
-    getBaseItemBox: function(containerBox){
-        var cH = containerBox.height,
-            cW = containerBox.width,
-            sizeFactor = (cW > cH) ? 0.68 : 0.52,
-            h, w;
-
-        h = w = Math.min(containerBox.width, containerBox.height) * sizeFactor; 
-
-        return {
-            top: 40  ,
-            height: h * 1.5, 
-            width: w,
-            left: (containerBox.width - w) / 2 
-        };
-    },
-
-    setBoundaries: function(itemBox){
-        var w = itemBox.width;
-        if(this.getFlat()){
-            this.gap = w * 1.1;
-            this.threshold = this.gap / 3; 
-            this.delta = w * 0.2;
-        } else {
-            this.gap = w / 3;
-            this.threshold = this.gap / 2; 
-            this.delta = w * 0.4;
-        }
-    },
-
-    setItemTransformation: Ext.emptyFn,
-
-    setItemTransform3d: function(item, idx, offset){
-        var x = idx * this.gap,
-            ix = x + offset,
-            transf = "";
-        if(ix < this.threshold && ix >= - this.threshold){
-            transf = "translate3d("+x+"px, 0, 150px)"
-            this.selectedIndex = idx;
-        }else if(ix > 0){
-            transf = "translate3d("+(x+this.delta)+"px, 0, 0) rotateY(-"+this.getAngle()+"deg)"
-        }else{
-            transf = "translate3d("+(x-this.delta)+"px, 0, 0) rotateY("+this.getAngle()+"deg)"
-        }   
-        item.dom.style.webkitTransform = transf;
-    },
-
-    setItemTransformFlat: function(item, idx, offset){
-        var x = idx * this.gap,
-            ix = x + offset,
-            transf = "";
-        if(ix < this.threshold && ix >= - this.threshold){
-            transf = "translate3d("+x+"px, 0, 150px)"
-            this.selectedIndex = idx;
-        }else if(ix > 0){
-            transf = "translate3d("+(x+this.delta)+"px, 0, 0)"
-        }else{
-            transf = "translate3d("+(x-this.delta)+"px, 0, 0)"
-        }   
-        item.dom.style.webkitTransform = transf;
-    },
-
-
-    doRefresh: function(me){
-        var container = me.container,
-            items, idx = 0, l,
-            orientation = Ext.Viewport.getOrientation();
-        
-        this.setOrientation(orientation);    
-
-        this.callParent([me]);
-        
-        items = container.getViewItems();
-        l = items.length;
-
-        this.itemBox = this.getBaseItemBox(this.element.getBox());
-        this.setBoundaries(this.itemBox);
-        
-        for(;idx<l;idx++){
-            this.resizeItem(items[idx]);
-        }
-
-        this.setSelectedIndex(this.selectedIndex);
-    },
-    
-    resizeItem: function(element){
-        var itemBox = this.itemBox,
-            item = Ext.get(element);
-            
-        item.setBox(itemBox);
-        /**
-            itemBox has an extra long in height to avoid reflection opacity over other items
-            I need to create a wrapper element with same bg to avoid that issue.
-        */
-        item.down('.'+this.getItemBaseCls()).setBox({height: itemBox.height/1.5, width: itemBox.width});
-    },
-    
-    //override
-    onStoreUpdate: function(store, record, newIndex, oldIndex) {
+    applyStore : function(store) {
         var me = this,
-            container = me.container,
-            item;
-        oldIndex = (typeof oldIndex === 'undefined') ? newIndex : oldIndex;
+            eventBind = Ext.apply(me.storeEvents, {scope: me});
 
-        if (oldIndex !== newIndex) {
-            container.moveItemsToCache(oldIndex, oldIndex);
-            container.moveItemsFromCache([record]);
-        }
-        else {
-            item = container.getViewItems()[newIndex];
-            // Bypassing setter because sometimes we pass the same record (different data)
-            container.updateListItem(record, item);
-            me.resizeItem(item);
+        if (store) {
+            store = Ext.data.StoreManager.lookup(store);
+            if (store && store.isStore) {
+                store.on(eventBind);
+            }
+        }  
 
+        return store;          
+    },
+
+
+    updateStore : function(store, oldStore) {
+        var me = this,
+            eventBind = Ext.apply(me.storeEvents, {scope: me});
+
+        if (oldStore && oldStore.isStore) {
+            oldStore.un(eventBind);
         }
+    },
+
+    applyItemTpl: function(config) {
+        return (Ext.isObject(config) && config.isTemplate) ? config : new Ext.XTemplate(config);
+    },
+
+    // -- Store Event Listeners
+
+    onStoreRefresh: function(){
+        console.log('refresh', arguments);
+        var me = this,
+            store = me.getStore(),
+            records = store.getRange();
+
+        me.addRecords(records);
+    },
+
+    onStoreBeforeLoad: function(){
+        console.log('beforeload', arguments);
+    },
+
+    onStoreLoad: function(){
+        console.log('load', arguments);
+    },
+
+    onStoreAdd: function(store, records){
+        console.log('add', arguments);
+    },
+
+    onStoreRemove: function() {
+      console.log('remove', arguments);
+    },
+
+    //-- end Store Event Listeners
+
+    addRecords: function(records) {
+        var me = this,
+            l = records.length,
+            i = 0, item;
+
+        for (; i < l; i ++) {
+            item = me.prepareItem(records[i]);
+            me.list.add(item);
+        }
+    },
+
+    prepareItem : function(record) {
+        var me = this,
+            itemTpl = me.getItemTpl();
+        return {
+            xtype: 'component',
+            html : itemTpl.apply(me.prepareData(record))
+        }
+    },
+
+    prepareData : function(record) {
+        return record.getData();
     }
-});
 
+
+    
+
+
+});
